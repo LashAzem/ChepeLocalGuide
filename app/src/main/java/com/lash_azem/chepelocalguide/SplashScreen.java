69ec35774203a8b722b718ab88e0f60bb95b80c9
Dagoberto Meza <dagoberto.meza@spacelab.cr>
2016-07-15 14:40:38 -0600

    Se agrego el > Welcome Screen

---
+++ app/src/main/java/com/lash_azem/chepelocalguide/SplashScreen.java
@@ -0,0 +1,7 @@
+package com.lash_azem.chepelocalguide;
+
+/**
+ * Created by Lash_Azem on 7/14/16.
+ */
+public class SplashScreen {
+}
---
+++ app/src/main/java/com/lash_azem/chepelocalguide/WelcomeActivity.java
@@ -0,0 +1,7 @@
+package com.lash_azem.chepelocalguide;
+
+/**
+ * Created by Lash_Azem on 7/15/16.
+ */
+public class WelcomeActivity {
+}
