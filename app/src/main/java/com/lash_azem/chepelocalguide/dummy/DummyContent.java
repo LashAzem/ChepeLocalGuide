03c2eaca13c964f1abd58edfdda6b9ed2e38e1e5
Dagoberto Meza <dagoberto.meza@spacelab.cr>
2016-07-18 23:30:24 -0600

    Update con nuevo contenido > Asignadas las imágenes correctas.

--- app/src/main/java/com/lash_azem/chepelocalguide/dummy/DummyContent.java
+++ app/src/main/java/com/lash_azem/chepelocalguide/dummy/DummyContent.java
@@ -7,12 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- * <p/>
- * TODO: Replace all uses of this class before publishing your app.
- */
 public class DummyContent {
 
     /**
@@ -28,36 +22,68 @@ public class DummyContent {
     //private static final int COUNT = 25;
 
     static {
-        addItem(new DummyItem("1","Leños", "¿Anteojos de madera? ¿Por qué no? La madera demuestra su gran versatilidad y ese toque natural del cual carecen otros materiales.", R.drawable.local_dummy));
-        addItem(new DummyItem("2", "Mar Rojo", "The latest fashion trends worldwide, in Costa Rica. ONLINE & MOBILE BOUTIQUE", R.drawable.local_dummy));
-        addItem(new DummyItem("3", "Wear Bona Fide", "Fabric Necklaces Handmade", R.drawable.local_dummy));
-        addItem(new DummyItem("4", "Tijo", "Tijo es una marca costarricense de ropa. Nuestros productos son originales y cada prenda es exclusiva y numerada.", R.drawable.local_dummy));
-        addItem(new DummyItem("5", "Plivertees", "Camisas Serigrafia", R.drawable.local_dummy));
-        addItem(new DummyItem("6", "Boza", "Bolsos de Cuero", R.drawable.local_dummy));
-        addItem(new DummyItem("7", "Tienda Eñe", "Tienda de Variedades", R.drawable.local_dummy));
-        addItem(new DummyItem("8", "Cafe Miel", "Cafeteria", R.drawable.local_dummy));
-        addItem(new DummyItem("9", "De Aqui y de Alla", "Tienda de variedades", R.drawable.local_dummy));
-        addItem(new DummyItem("10", "11:11 Banda", "Banda Costarricense", R.drawable.local_dummy));
-        addItem(new DummyItem("11", "NOMELLAMO", "Taller de creacion de Peluches", R.drawable.local_dummy));
-        addItem(new DummyItem("12", "Aromaflor", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.", R.drawable.local_dummy));
-        addItem(new DummyItem("13", "Nomada", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.", R.drawable.local_dummy));
-        addItem(new DummyItem("14", "Trash", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.", R.drawable.local_dummy));
-        addItem(new DummyItem("15", "Va de Nuevo", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.", R.drawable.local_dummy));
-        addItem(new DummyItem("16", "Pez", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.", R.drawable.local_dummy));
-        addItem(new DummyItem("17", "Cabeza de vinil Banda", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaq", R.drawable.local_dummy));
-        addItem(new DummyItem("18", "Gato Negro", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaq", R.drawable.local_dummy));
-        addItem(new DummyItem("19", "Nabruk", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaq", R.drawable.local_dummy));
+        addItem(new DummyItem("1",
+                "Leños",
+                "¿Anteojos de madera? ¿Por qué no? La madera demuestra su gran versatilidad y ese toque natural del cual carecen otros materiales.",
+                R.drawable.local_lenos));
+        addItem(new DummyItem("2",
+                "Wear Bona Fide",
+                "Collares de Tela Bona Fide, Diseñados y hechos a mano en Costa Rica",
+                R.drawable.local_bonafide));
+        addItem(new DummyItem("4",
+                "Tijo",
+                "Tijo es una marca costarricense de ropa. Nuestros productos son originales y cada prenda es exclusiva y numerada.",
+                R.drawable.local_tijo));
+        addItem(new DummyItem("5",
+                "Plivertees",
+                "Es una marca de StreetWear para hombre y mujer creados por el artista de Costa Rica Oliver Skinner en 2004.",
+                R.drawable.local_pliver));
+        addItem(new DummyItem("10",
+                "11:11",
+                "Es la búsqueda del balance, cuando todo está alineado. Es también la forma en que seres de otras dimensiones se comunican, los números y como resuenan en nosotros cuando se alinean en sus diferentes formas. La música es un regalo del universo. ",
+                R.drawable.local_onceonce));
+        addItem(new DummyItem("11",
+                "NOMELLAMO",
+                "Taller de creacion de Peluches. En nuestro laboratorio de monstruos y experimentos, surgen los NOMELLAMO, criaturas adorables, cargadas de intensidad, color, textura y particularidades que exaltan la personalidad y ternura que los caracteriza. ",
+                R.drawable.local_nomellamo));
+        addItem(new DummyItem("12",
+                "Aromaflor",
+                "Productos orgánicos certificados hechos en Costa Rica. Extractos botánicos y minerales con formulación profesional.",
+                R.drawable.local_aromaflor));
+        addItem(new DummyItem("13",
+                "Nomada",
+                "Si nos bañamos y secamos todos los días por qué no con arte y diseño?Useful -inspiring day a day designed paños con frases e imágenes que le shift el día.",
+                R.drawable.local_nomada));
+        addItem(new DummyItem("14",
+                "Trash",
+                "eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.",
+                R.drawable.local_dummy));
+        addItem(new DummyItem("15",
+                "Va de Nuevo",
+                "Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.",
+                R.drawable.local_dummy));
+        addItem(new DummyItem("16",
+                "Pez",
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+                R.drawable.local_pez));
+        addItem(new DummyItem("17",
+                "Cabeza de vinil Banda",
+                "Sed ut perspiciatis unde omnis iste",
+                R.drawable.local_dummy));
+        addItem(new DummyItem("18",
+                "Gato Negro",
+                "laudantium, totam rem aperiam, eaq",
+                R.drawable.local_dummy));
+        addItem(new DummyItem("19",
+                "Nabruk",
+                "Doloremque laudantium, totam rem aperiam, eaq",
+                R.drawable.local_dummy));
     }
 
     private static void addItem(DummyItem item) {
         ITEMS.add(item);
         ITEM_MAP.put(item.id, item);
     }
-
-
-    /**
-     * A dummy item representing a piece of content.
-     */
     public static class DummyItem {
         public final String id;
         public final String name;
