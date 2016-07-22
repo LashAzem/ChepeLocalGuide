3d72908f662fef005776dc80e9566aba87b387c5
Dagoberto Meza <dagoberto.meza@spacelab.cr>
2016-07-21 22:01:20 -0600

    Folder Notification

---
+++ app/src/main/java/com/lash_azem/chepelocalguide/notifications/PushNotificationContract.java
@@ -0,0 +1,32 @@
+package com.lash_azem.chepelocalguide.notifications;
+
+import com.herprogramacion.carinsurance.BasePresenter;
+import com.herprogramacion.carinsurance.BaseView;
+import com.herprogramacion.carinsurance.data.PushNotification;
+
+import java.util.ArrayList;
+
+/**
+ * Interacción MVP en Notificaciones
+ */
+public interface PushNotificationContract {
+
+    interface View extends BaseView<Presenter>{
+
+        void showNotifications(ArrayList<PushNotification> notifications);
+
+        void showEmptyState(boolean empty);
+
+        void popPushNotification(PushNotification pushMessage);
+    }
+
+    interface Presenter extends BasePresenter{
+
+        void registerAppClient();
+
+        void loadNotifications();
+
+        void savePushMessage(String title, String description,
+                             String expiryDate, String discount);
+    }
+}
