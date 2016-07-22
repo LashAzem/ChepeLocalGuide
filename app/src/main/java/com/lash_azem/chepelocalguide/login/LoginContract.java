f75be64dffb73747d8bb2fe45db323fd798b2934
Dagoberto Meza <dagoberto.meza@spacelab.cr>
2016-07-21 21:47:29 -0600

    Volder Login + Archivos Login

---
+++ app/src/main/java/com/lash_azem/chepelocalguide/login/LoginContract.java
@@ -0,0 +1,32 @@
+package com.lash_azem.chepelocalguide.login;
+
+import com.herprogramacion.carinsurance.BasePresenter;
+import com.herprogramacion.carinsurance.BaseView;
+
+/**
+ * Interacción MVP en Login
+ */
+public interface LoginContract {
+
+    interface View extends BaseView<Presenter>{
+        void showProgress(boolean show);
+
+        void setEmailError(String error);
+
+        void setPasswordError(String error);
+
+        void showLoginError(String msg);
+
+        void showPushNotifications();
+
+        void showGooglePlayServicesDialog(int errorCode);
+
+        void showGooglePlayServicesError();
+
+        void showNetworkError();
+    }
+
+    interface Presenter extends BasePresenter{
+        void attemptLogin(String email, String password);
+    }
+}
